3719f2839c1d84390ecce6881bab29c1750a2f6a
Sid <sid6327@gmail.com>
2017-09-09 23:53:52 +0530

    Adding a problem on graph.

---
+++ Graph Algorithms/DisjointSet.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int VISITED[30006];
+vector < vector <int> > ADJ_LIST(30006); 
+
+int bfs (int start) {
+	queue <int> Q;
+	Q.push(start);
+	VISITED[start] = 1;
+	int connectedComponents = 0;
+	while (!Q.empty()) {
+		int node = Q.front();
+		connectedComponents++;
+		Q.pop();
+		for (int i = 0; i < ADJ_LIST[node].size(); i++) {
+			if (!VISITED[ADJ_LIST[node][i]]) {
+				Q.push(ADJ_LIST[node][i]);
+				VISITED[ADJ_LIST[node][i]] = 1;
+			}
+		}
+	}
+	return connectedComponents;
+}
+
+int main () {
+	int n, g, b, max = 0, min = 30006;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> g >> b;
+		ADJ_LIST[g].push_back(b);
+		ADJ_LIST[b].push_back(g);
+	}
+	for (int i = 1; i <= n; i++) {
+		if (!VISITED[i] && ADJ_LIST[i].size()) {
+			int connectedComponents = bfs(i);
+			if (connectedComponents > max) {
+				max = connectedComponents;
+			}
+			if (connectedComponents < min) {
+				min = connectedComponents;
+			}
+		}
+	}
+	cout << min << " " << max;
+	return 0;
+}

67bbc32028f9bc92561610efc6e05ca06af4cd01
Sid <sid6327@gmail.com>
2017-09-14 23:22:12 +0530

    Adding code on bleak numbers.

---
+++ Ad-hoc/BleakNumber.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int getSetBits(long long unsigned int num) {
+    long long unsigned int mask = 1 << 16;
+    int ans = 0;
+    while (mask) {
+        if (mask & num) {
+            ans++;
+        }
+        mask = (mask>>1);
+    }
+    return ans;
+}
+
+int main() {
+	//code
+	long long unsigned int n, t;
+	cin >> t;
+	while (t--) {
+	    cin >> n;
+	    int start = 0, ans = 1;
+	    if (n >= 16) {
+	        start = n - 16;
+	    }
+	    for (long long unsigned int i = start; i <= n; i++) {
+	        if (i + getSetBits(i) == n) {
+	            ans = 0;
+	            break;
+	        }
+	    }
+	    cout << ans << "\n";
+	}
+	return 0;
+}

ff0581c0df55e0a2e1c8f9a8c4cb2c131c593aaa
Siddharth Chopra <sid6327@gmail.com>
2017-10-02 23:34:54 +0530

    Fixed with better concatenation.

--- Ad-hoc/StringCompression.cpp
+++ Ad-hoc/StringCompression.cpp
@@ -1,39 +1,32 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <cstring>
+#include <cstdio>
 
 using namespace std;
 
 int main() {
 	string str;
-	char compress[30];
+	char compress[30], countString[33];
 	cin >> str;
 	char prev = '1';
 	int count = 1, j = 0, len = 0;
 	for (int i = 0; i < str.length(); i++) {
 		if (str[i] == prev) {
 			count++;
-			if (count > 2) {
-				j -= len;
-			}
-			int temp = count;
-			len = ceil(log10(count));
-			while (temp) {
-				int digit = temp % 10;
-				// if (count == 10) {
-				// 	cout << "temp = " << temp << ", dig = " << digit << "\n";
-				// 	cout << j + len - 1 << " = " << (char)(digit + 48) << "\n";
-				// }
-				temp = temp / 10;
-				compress[j + len - 1] = (char)(digit + 48);
-				j++;
-			}
 		} else {
+			sprintf(countString, "%d", count);
+			strcat(compress, countString);
 			count = 1;
+			j += strlen(countString);
 			prev = str[i];
 			compress[j++] = str[i];
 		}
 	}
+	sprintf(countString, "%d", count);
+	strcat(compress, countString);
+	j += strlen(countString);
 	compress[j] = '\0';
 	cout << compress;
 	return 0;
